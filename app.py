4724e7a78bbd84581a695e98556cf431cb49075a
Minh Nhat <wunnynhatminh2602@gmail.com>
2024-05-09 11:16:47 +0700

    first

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
+import os
+from dotenv import dotenv_values
+
+env_config = dotenv_values('.env')
+
+value = os.environ.get('IO_KEY')
+
+value = os.environ['IO_KEY'] if ('IO_KEY' in os.environ) else env_config['IO_KEY']
+
+print(value)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    return render_template('index.html', title=value)
 
 if __name__ == '__main__':
     app.run(debug=True)
